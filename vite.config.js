f523e13f0ed5be4015f12309593793db146727a1
epointal <elisabeth.pointal@free.fr>
2025-03-20 09:43:18 +0100

    elements de configuration pour build

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,20 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
-
+const PACKAGE = require('./package.json')
+const prodUrl = PACKAGE.production.url + '/' + PACKAGE.name + '@' + PACKAGE.version +  '/dist/'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
   ],
+  experimental: {
+	  renderBuiltUrl(filename, hostType) {
+	    
+	      return prodUrl + '/' + filename
+	  }
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
