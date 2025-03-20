28f8a9d340dcdec62551bc8578f8ba225b03f28a
epointal <elisabeth.pointal@free.fr>
2025-03-20 14:56:17 +0100

    test css parametrage

--- src/stores/config.js
+++ src/stores/config.js
@@ -2,7 +2,12 @@ import { defineStore } from 'pinia'
 
 export const useConfig = defineStore('config', () => {
   let state = {
-      api: 'https://machin'
+      geonetwork: 'https://catalogue-terresolide.ipgp.fr/geonetwork',
+      primary:"#754a15",
+      emphasis: "#dd9946",
+      link: "#754a15",
+      over: "#8c0209"
+
   }
   function init(conf) {
     state = Object.assign(state, conf)
