b62f3506de06edcf4cd31bf4257234e2d105ad6b
epointal <elisabeth.pointal@free.fr>
2025-04-09 10:12:09 +0200

    footer des cartouches

---
+++ src/components/RelatedLink.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { computed} from 'vue';
+import {useConfig} from '@/stores/config'
+const props = defineProps({
+   links: Object,
+   catalogId: Number
+})
+let config = useConfig()
+
+</script>
+<template>
+
+  
+</template>
+<style scoped>
+
+</style>
