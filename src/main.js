9caa97077d6234933713010c74e1c9c047ff0af7
epointal <elisabeth.pointal@free.fr>
2025-04-28 13:02:59 +0200

    le main git add main.js !

---
+++ src/main.js
@@ -0,0 +1,49 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import { createPinia, defineStore } from 'pinia'
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+/* import specific icons */
+import {faAsterisk,faCaretDown,faCircleDot,faDownload, faFile, faFolderOpen, faEarthAmericas, faGaugeSimpleHigh, faGrip, faLink, faMagnifyingGlass,faMap, 
+        faPenToSquare,faSatelliteDish, faTerminal, faUser} from '@fortawesome/free-solid-svg-icons'
+import { setupI18n, loadLocaleMessages } from './i18n'
+import moment from 'moment'
+import fr from "moment/dist/locale/fr"
+// import {
+//   faSquare as farSquare
+// } from '@fortawesome/free-regular-svg-icons'
+library.add(faAsterisk,faCaretDown, faCircleDot,faDownload, faFile, faFolderOpen,faEarthAmericas, faGaugeSimpleHigh, faGrip, faLink, faMagnifyingGlass,faMap,
+            faPenToSquare,faSatelliteDish,faTerminal, faUser)
+
+let cfg = {}
+
+if (typeof catalog != 'undefined') {
+  cfg = JSON.parse(catalog.innerHTML)
+} 
+
+let locale = navigator.language.substr(0, 2)
+if (!cfg.lang) {
+  cfg.lang = locale === 'fr' ? 'fr' : 'en'
+} 
+
+moment.locale(cfg.lang)
+
+console.log(cfg.lang)
+const i18n = setupI18n({locale: cfg.lang})
+loadLocaleMessages(i18n, cfg.lang)
+import App from './App.vue'
+import router from './router'
+import {useConfig} from '@/stores/config'
+const app = createApp(App)
+app.component('font-awesome-icon', FontAwesomeIcon)
+app.use(createPinia())
+app.use(router)
+app.use(i18n)
+app.provide('moment', moment)
+
+const config = useConfig()
+config.init(cfg)
+app.mount('#app')
