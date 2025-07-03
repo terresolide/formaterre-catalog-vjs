4c2450585c2a6e6c3eb24488f19d33456ec2dffe
epointal <elisabeth.pointal@free.fr>
2025-07-03 11:01:13 +0200

    dimension merge resultat

--- src/components/DimensionBlock.vue
+++ src/components/DimensionBlock.vue
@@ -1,5 +1,5 @@
 <script setup>
-import {computed, reactive} from 'vue'
+import {computed, reactive, watch} from 'vue'
 import {useRoute, useRouter} from 'vue-router'
 import {useCatalog} from '@/stores/catalog'
 const {name, aggregation} = defineProps({
@@ -16,6 +16,9 @@ const route = useRoute()
 const router = useRouter()
 const catalog = useCatalog()
 
+const data = reactive({
+    dimensions: {}
+})
 const dimensions = computed(() => {
     aggregation.category.sort((a, b) => {
         if (a.label > b.label) {
@@ -57,6 +60,20 @@ function select(key) {
     router.push({name: route.name, params: route.params, query: query})
     
 }
+watch(() => aggregation,
+    agg => {
+        if (agg.reset) {
+            data.aggregation = aggregation
+            data.aggregation.category.sort((a, b) => {
+                if (a.label > b.label) {
+                    return 1
+                }
+                return -1
+            })
+        } else {
+            // merge agg avec data.aggregation
+        }
+})
 </script>
 <template>
     <div v-for="dim in dimensions.category" @click="select(dim.key)">
