3989a4d48f80a3ea56e96cb99185ef6ade16a3ed
epointal <elisabeth.pointal@free.fr>
2025-08-21 09:53:26 +0200

    exportLinks

--- src/components/ExportLinks.vue
+++ src/components/ExportLinks.vue
@@ -1,5 +1,5 @@
 <script setup>
-const exportLinks = defineProps({
+const {exportLinks} = defineProps({
     exportLinks: {
         type: Object,
         default: null
@@ -8,34 +8,49 @@ const exportLinks = defineProps({
 
 </script>
 <template >
-{{exportLinks}}
  <span class="mtdt-export-links" v-if="exportLinks.xml || exportLinks.pdf || exportLinks.json">
   <div  class="mtdt-tab-export">
-    <i class="fa fa-chevron-down"></i>
-      <span style="">{{$t('export')}}</span>
+     Export  
+     <font-awesome-icon icon="fa-solid fa-caret-down" /> 
+           
+     
   </div>
   <div class="mtdt-expand">
-     <div v-if="exportLinks.json"><a :href="exportLinks.json" target="_blank">{{$t('exportJSON')}}</a></div>
-     <div v-if="exportLinks.xml"><a :href="exportLinks.xml" target="_blank">{{$t('exportXML')}}</a></div>
-     <div v-if="exportLinks.pdf"><a :href="exportLinks.pdf" target="_blank">{{$t('exportPDF')}}</a></div>
+     <div v-if="exportLinks.json"><a :href="exportLinks.json" target="_blank">Export JSON</a></div>
+     <div v-if="exportLinks.xml"><a :href="exportLinks.xml" target="_blank">Export XML</a></div>
+     <div v-if="exportLinks.pdf"><a :href="exportLinks.pdf" target="_blank">Export PDF</a></div>
   </div>
  </span>
 </template>
 
 <style scoped>
 .mtdt-export-links{
+  color: var(--color-text);
   float:right;
   margin-right:10px;
+  position:relative;
+  cursor: pointer;
 }
 .mtdt-tab-export{
   min-width: 80px;
   text-align:center;
+  padding: 5px 10px;
+  border: 1px dotted grey;
+    border-top-width: 1px;
+    border-top-style: dotted;
+    border-top-color: grey;
+  border-top: 0px;
+  /* background: #eee;*/
+  cursor: pointer;
 }
 .mtdt-export-links div.mtdt-expand{
   display: none;
   text-align: center;
   position:absolute;
   min-width:102px;
+  margin:0;
+  right:0px;
+  top:35px;
   z-index: 10;
 }
 .mtdt-export-links div.mtdt-expand div{
@@ -46,14 +61,14 @@ const exportLinks = defineProps({
    border-top:0px;
 }
 .mtdt-export-links a{
-   color: black;
+   color:var(--color-text);
    text-decoration:none;
 }
 .mtdt-export-links div.mtdt-expand div:hover{
    background: #ccc;
 }
-.mtdt-tab-export:hover + div.mtdt-expand,
-.mtdt-export-links .mtdt-expand:hover{
+
+.mtdt-export-links:hover div.mtdt-expand{
   display: block;
 }
 </style>
