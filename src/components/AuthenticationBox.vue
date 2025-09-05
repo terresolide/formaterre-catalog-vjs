ddc65812a8587ae4660e81c3e19d652b2548d013
epointal <elisabeth.pointal@free.fr>
2025-09-05 15:17:32 +0200

    test avec formater-auth-service-js

--- src/components/AuthenticationBox.vue
+++ src/components/AuthenticationBox.vue
@@ -1,11 +1,26 @@
 <script setup>
-import {onMounted} from 'vue'
+import {reactive, onMounted} from 'vue'
+import {AuthService} from 'formater-auth-service-js'
+
+const data = reactive({
+    sso: null
+})
+function login () {
+    data.sso.login()
+}
 onMounted(() => {
     console.log(import.meta.env)
+    var env = import.meta.env
+    data.sso = new AuthService(env.SSO_NAME, {
+           keycloakUrl: env.SSO_URL + '/realms/' + env.SSO_REALM,
+           redirectUri: 'http://localhost:3000/#/login'
+    })
+    console.log(data.sso)
+    
 })
 </script>
 <template> 
-    <a><font-awesome-icon icon="fa-solid fa-user" /> {{ $t('login') }}</a>
+    <a @click="login"><font-awesome-icon icon="fa-solid fa-user" /> {{ $t('login') }}</a>
 </template>
 <style>
 </style>
