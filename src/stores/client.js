85283c4fbbf972a5c37734ec4332e841e076a6c7
epointal <elisabeth.pointal@free.fr>
2025-09-10 09:47:19 +0200

    auth + roles

--- src/stores/client.js
+++ src/stores/client.js
@@ -3,13 +3,21 @@ import { defineStore } from 'pinia'
 export const useClient = defineStore('client', {
     state: () => ({
         list:[],
+        loaded: false,
         current: null
         
     }),
     actions: {
         async getAll () {
-           const config = useConfig()
-           const response = await fetch(config.state.tools + '/api/client/' + config.state.app)
+            const config = useConfig()
+           
+            const response = await fetch(config.state.tools + '/api/client/' + config.state.app)
+            // const response = await fetch(config.state.tools + 'requests/check', {
+            //     userId:,
+            //     email:,
+            //     app:,
+            //     realm: 
+            // })
             
         },
         reset () {
