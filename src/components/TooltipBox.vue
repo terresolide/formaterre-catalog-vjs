1fc0f792b33f36fa81abcbb1f2656351fd820ed0
epointal <elisabeth.pointal@free.fr>
2025-09-11 16:43:10 +0200

    continue tooltip

--- src/components/TooltipBox.vue
+++ src/components/TooltipBox.vue
@@ -2,14 +2,20 @@
 const {description} = defineProps({
     icon: {
         type: String,
-        default: 'fa-solid fa-'
+        default: 'fa-solid fa-info-circle'
     },
     description: null
 })
 </script>
 <template v-if="description">
-  <div>i</div>
-  <div v-html="description" />
+ <div class="tooltip-container" style="position:relative;">
+      <span class="fa-solid fa-info-circle" @click="showTooltip($event)"></span>
+      <div class="fmt-tooltip" @click="hideTooltip()" v-html="description"></div>
+</div> 
 </template>
 <style scoped>
+.tooltip-container {
+    display:inline-block;
+    position: relative;
+}
 </style>
