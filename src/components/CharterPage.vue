75586da4a2ed4b02db72f59a1cec6630db8d0516
epointal <elisabeth.pointal@free.fr>
2025-09-17 17:25:26 +0200

    affiche pdf charte

--- src/components/CharterPage.vue
+++ src/components/CharterPage.vue
@@ -1,19 +1,35 @@
 <script setup>
-import {computed} from 'vue'
+import {computed, reactive, ref} from 'vue'
 import {useConfig} from '@/stores/config.js'
 import {useSelection} from '@/stores/selection.js'
 import {useClient} from '@/stores/client.js'
+import VuePdfEmbed from 'vue-pdf-embed'
+
 const {include} = defineProps({
     include: {
         type: Boolean,
         default: true
     }
 })
+const pdfRef = ref()
+const data = reactive({
+  isLoading: true,
+  page: null,
+  pageCount: null
+})
 const config = useConfig()
 const client = useClient()
 const selection = useSelection()
 const charter = computed(() => {
    return client.charters.list.find(ch => ch.id === selection.charter)
+})
+const url = computed(() => {
+    console.log(charter.value.file)
+    if (!charter || !charter.value.file) {
+        
+        return null
+    }
+    return config.state.tools + '/pdf/' + charter.value.file[config.state.lang]
 })
 function close() {
     if (include) {
@@ -22,20 +38,68 @@ function close() {
         // back
     }
 }
+function handleDocumentLoad(result) {
+      data.pageCount = result.numPages
+      data.page = 1
+}
+function  handleDocumentRender() {
+      data.isLoading = false
+}
+function download () {
+  pdfRef.value.download(charter.title[config.state.lang])
+}
+function print() {
+  pdfRef.value.print(90, charter.title[config.state.lang], true)
+}
 </script>
 <template>
    <template v-if="selection.charter">
+
    <div class="charter" :class="{include: include}">
         <h2 :style="{backgroundColor: config.state.primary}">
             <div class="close" @click="close">&times;</div>
-            <template v-if="charter">
-               {{charter.title[config.state.lang]}}
-            </template>
-            <template v-else>
+                <template  v-if="url">
+                   {{charter.title[config.state.lang]}}
+                </template>
+                 <template v-else>
                CHARTER NOT FOUND
             </template>            
         </h2>
-     {{charter}}
+        <template v-if="url">
+            <div style="border:1px solid black;max-width:900px;margin:auto;">
+             <div class="app-header">
+                <template v-if="data.isLoading">
+                  Loading...
+                </template>
+            
+                <template v-else>
+                    <div> &nbsp;</div>
+                    <div style="text-align:center;">
+                      <button class="classic" :disabled="data.page <= 1" @click="data.page--">❮</button>
+                    
+                      {{ data.page }} / {{ data.pageCount }} &nbsp;
+                    
+                      <button class="classic" :disabled="data.page >= data.pageCount" @click="data.page++">❯</button>
+                    
+                    </div>
+                    <div style="text-align:right;" >
+                        <button class="classic" @click="download" style="padding:3px 10px;">
+                          <font-awesome-icon icon="fa-download"></font-awesome-icon>
+                        </button>
+                        <button class="classic" @click="print" style="padding:3px 10px;">
+                          <font-awesome-icon icon="fa-print"></font-awesome-icon>
+                         </button>
+                    </div>
+                   
+            </template>
+            
+            </div>
+            <vue-pdf-embed ref="pdfRef" :source="url" :page="data.page" 
+           @loaded="handleDocumentLoad" @rendered="handleDocumentRender" />
+           </div>
+        </template>
+           
+}
    </div>
    </template>
 </template>
@@ -79,5 +143,18 @@ div.close {
 
 div.close:hover {
     border-color:white;
+}
+.vue-pdf-embed > div {
+  margin-bottom: 8px;
+  box-shadow: 0 2px 8px 4px rgba(0, 0, 0, 0.1);
+}
+
+.app-header {
+  padding: 16px;
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  box-shadow: 0 2px 8px 4px rgba(0, 0, 0, 0.1);
+  background-color: #555;
+  color: #ddd;
 }
 </style>
