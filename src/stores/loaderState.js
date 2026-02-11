67c387271e407c17d7beeb3d793cb40c595a390c
epointal <elisabeth.pointal@free.fr>
2026-02-11 13:15:27 +0100

    oublie fichier

---
+++ src/stores/loaderState.js
@@ -0,0 +1,15 @@
+import { defineStore } from 'pinia'
+import {ref} from 'vue'
+export const useLoaderState = defineStore('isLoading', () => {
+    const state = ref(false)
+
+    const changeStateTrue = () => {
+        state.value = true
+    }
+
+    const changeStateFalse = () => {
+        state.value = false
+    }
+
+    return { state, changeStateTrue, changeStateFalse }
+})
