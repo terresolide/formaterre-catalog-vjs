a90559e85eff2ea672c00285428c632250849a2c
epointal <pointal@ipgp.fr>
2026-08-06 15:52:37 +0200

    bug no resourceProvider

--- src/modules/metadataConverter.js
+++ src/modules/metadataConverter.js
@@ -91,12 +91,14 @@ export default function (attrs) {
         if (catalog.organismThesaurus && catalog.organismThesaurus.th_name.endsWith('OrgForResourceObject')) {
                 metadata.dataCenter = []
                 var role = catalog.organismThesaurus.th_name.replace('OrgForResourceObject', '')
+                if (contacts[role]) {
                 var cts = contacts[role].filter(x => x[8])
-                cts.forEach(function (ct) {
-                    if (catalog.organisms[ct[8]]) {
-                      metadata.dataCenter.push(catalog.organisms[ct[8]])
-                    }
-                })
+                  cts.forEach(function (ct) {
+                      if (catalog.organisms[ct[8]]) {
+                        metadata.dataCenter.push(catalog.organisms[ct[8]])
+                      }
+                  })
+                }
             }
          var contacts = extractContacts(
             JSONPATH.query(json['gmd:contact'], "$..['gmd:CI_ResponsibleParty']"), idLang)
